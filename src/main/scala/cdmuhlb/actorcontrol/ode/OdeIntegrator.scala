e7be12f52e47dd8c6809307cc79be83c056121df
Curran D. Muhlberger <curran@muhlbergerweb.com>
2013-10-20 15:32:28 -0400

    Implement ConstantDtIntegrator

--- src/main/scala/cdmuhlb/actorcontrol/ode/OdeIntegrator.scala
+++ src/main/scala/cdmuhlb/actorcontrol/ode/OdeIntegrator.scala
@@ -4,6 +4,8 @@ trait OdeSolution[S <: OdeStep[Y, D], Y <: OdeYState[Y, D],
     D <: OdeDyDtState[D, Y]] {
   def containsTime(t: Double): Boolean
   def interpolate(t: Double): OdeState[Y, D]
+  def lastStep: S
+  def nSteps: Int
 }
 
 class OdeSolutionBuilder[S <: OdeStep[Y, D], Y <: OdeYState[Y, D],
@@ -23,6 +25,8 @@ class OdeSolutionBuilder[S <: OdeStep[Y, D], Y <: OdeYState[Y, D],
       require(stepOpt.nonEmpty)
       stepOpt.get.interpolate(t)
     }
+    def lastStep = steps.last
+    def nSteps = steps.length
 
     private def search(t: Double): Option[S] = {
       def binarySearch(lo: Int, hi: Int): Option[S] = {
@@ -44,4 +48,48 @@ class OdeSolutionBuilder[S <: OdeStep[Y, D], Y <: OdeYState[Y, D],
 trait OdeIntegrator[S <: OdeStep[Y, D], O <: Ode[Y, D], Y <: OdeYState[Y, D],
     D <: OdeDyDtState[D, Y]] {
   def integrate(state: OdeState[Y, D], t: Double): OdeSolution[S, Y, D]
+  def integrate(lastStep: S, t: Double): OdeSolution[S, Y, D]
+}
+
+
+class ConstantDtIntegrator[S <: OdeStep[Y, D], O <: Ode[Y, D],
+    Y <: OdeYState[Y, D], D <: OdeDyDtState[D, Y]](
+    stepper: OdeStepper[S, O, Y, D], maxDt: Double)
+    extends OdeIntegrator[S, O, Y, D] {
+  def integrate(state: OdeState[Y, D], t: Double) = {
+    require(t > state.t)
+    val duration = t - state.t
+    val nSteps = math.ceil(duration/maxDt).toInt
+    val dt = duration / nSteps
+    val builder = new OdeSolutionBuilder[S, Y, D]
+    if (nSteps == 1) builder.appendStep(stepper.step(state, t))
+    else {
+      val t1 = state.t + dt
+      var step = stepper.step(state, t1)
+      builder.appendStep(step)
+      for (i ← 2 until nSteps) {
+        val ti = state.t + i*dt
+        step = stepper.step(step, ti)
+        builder.appendStep(step)
+      }
+      builder.appendStep(stepper.step(step, t))
+    }
+    builder.toSolution
+  }
+
+  def integrate(lastStep: S, t: Double): OdeSolution[S, Y, D] = {
+    require(t > lastStep.endTime)
+    val duration = t - lastStep.endTime
+    val nSteps = math.ceil(duration/maxDt).toInt
+    val dt = duration / nSteps
+    val builder = new OdeSolutionBuilder[S, Y, D]
+    var step = lastStep
+    for (i ← 1 until nSteps) {
+      val ti = lastStep.endTime + i*dt
+      step = stepper.step(step, ti)
+      builder.appendStep(step)
+    }
+    builder.appendStep(stepper.step(step, t))
+    builder.toSolution
+  }
 }
--- src/test/scala/cdmuhlb/actorcontrol/ode/OdeIntegratorSpec.scala
+++ src/test/scala/cdmuhlb/actorcontrol/ode/OdeIntegratorSpec.scala
@@ -36,4 +36,74 @@ class OdeIntegratorSpec extends WordSpec with Matchers {
       sol.containsTime(t3 + 1.0) should be (false)
     }
   }
+
+  "An OdeSolution" should {
+    "correctly return the last step" in {
+      val t0 = 1.0
+      val t1 = 5.0
+      val t2 = 7.5
+      val state0 = OdeState[ScalarYState, ScalarDyDtState](t0,
+          ScalarYState(3.5))
+      val state1 = OdeState[ScalarYState, ScalarDyDtState](t1,
+          ScalarYState(4.0))
+      val state2 = OdeState[ScalarYState, ScalarDyDtState](t2,
+          ScalarYState(-3.0))
+      val step1 = LinearStep(state0, state1)
+      val step2 = LinearStep(state1, state2)
+
+      val builder = new OdeSolutionBuilder[
+          LinearStep[ScalarYState, ScalarDyDtState],
+          ScalarYState, ScalarDyDtState]
+      builder.appendStep(step1)
+      builder.appendStep(step2)
+
+      val sol = builder.toSolution
+      sol.lastStep should equal(step2)
+    }
+  }
+
+  "A ConstantDtIntegrator" should {
+    "compute linear solutions exactly with Euler's method" in {
+      val dydt = 3.0
+      val ode = new ConstantOde(dydt)
+      val stepper = new EulerMethod[ConstantOde, ScalarYState,
+          ScalarDyDtState](ode)
+      val t0 = 0.0
+      val y0 = 1.0
+      val state0 = OdeState[ScalarYState, ScalarDyDtState](t0, ScalarYState(y0))
+      val t1 = 5.0
+      val y1 = y0 + dydt*(t1 - t0)
+
+      val maxDt = 0.7
+      val solver = new ConstantDtIntegrator(stepper, maxDt)
+      val sol = solver.integrate(state0, t1)
+      val solState = sol.interpolate(t1)
+      solState.t should equal (t1)
+      solState.y.y should be (y1 +- 1.0e-15)
+
+      val minSteps = math.ceil((t1 - t0)/maxDt).toInt
+      sol.nSteps should be >= (minSteps)
+    }
+    "compute correct solutions when starting from an OdeStep" in {
+      val dydt = 3.0
+      val ode = new ConstantOde(dydt)
+      val stepper = new BogackiShampineMethod[ConstantOde, ScalarYState,
+          ScalarDyDtState](ode)
+      val t0 = 0.0
+      val y0 = 1.0
+      val state0 = OdeState[ScalarYState, ScalarDyDtState](t0, ScalarYState(y0))
+      val t1 = 0.1
+      val step1 = stepper.step(state0, t1)
+      val t2 = 5.0
+
+      val maxDt = 0.7
+      val solver = new ConstantDtIntegrator(stepper, maxDt)
+      val stepSol = solver.integrate(step1, t2)
+      val stateSol = solver.integrate(step1.endState, t2)
+      val stepSolState = stepSol.interpolate(t2)
+      val stateSolState = stateSol.interpolate(t2)
+      stepSolState.t should equal (t2)
+      stepSolState.y.y should be (stateSolState.y.y +- 1.0e-15)
+    }
+  }
 }
