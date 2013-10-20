8632d0c0c5609c391d91f48572b500eca863c432
Curran D. Muhlberger <curran@muhlbergerweb.com>
2013-10-20 12:00:52 -0400

    Add interface for error-measuring ODE steppers

--- src/main/scala/cdmuhlb/actorcontrol/ode/OdeStepper.scala
+++ src/main/scala/cdmuhlb/actorcontrol/ode/OdeStepper.scala
@@ -13,3 +13,16 @@ trait OdeStepper[S <: OdeStep[Y, D], O <: Ode[Y, D],
   def step(state: OdeState[Y, D], t: Double): S
   def step(seg: S, t: Double): S
 }
+
+trait ErrorMeasurer[Y <: OdeYState[Y, D], D <: OdeDyDtState[D, Y]] {
+  def measureError(y1: OdeState[Y, D], y2: OdeState[Y, D]): Double
+}
+
+trait EmbeddedOdeStepper[S <: OdeStep[Y, D], O <: Ode[Y, D],
+    Y <: OdeYState[Y, D], D <: OdeDyDtState[D, Y]]
+    extends OdeStepper[S, O, Y, D] {
+  def step(state: OdeState[Y, D], t: Double, measurer: ErrorMeasurer[Y, D]):
+      (S, Double)
+  def step(seg: S, t: Double, measurer: ErrorMeasurer[Y, D]):
+      (S, Double)
+}
