ca30ebc6432fec6e2e4331788ae38f3242f6a6c6
Curran D. Muhlberger <curran@muhlbergerweb.com>
2013-10-22 22:57:35 -0400

    Add sample driver code
    The main class now simulates and measures a pendulum until interrupted
    by console input.

--- src/main/scala/cdmuhlb/actorcontrol/Main.scala
+++ src/main/scala/cdmuhlb/actorcontrol/Main.scala
@@ -1,3 +1,28 @@
 package cdmuhlb.actorcontrol
 
-object Main extends App
+import akka.actor.{ActorSystem, Props}
+import akka.actor.{Actor, ActorLogging, ActorRef}
+
+object Main extends App {
+  val system = ActorSystem("ActorControl")
+  val mass = 1.0
+  val length = 1.0
+  val theta0 = 1.0
+  val world = new DeterministicWorld(mass, length, 
+      SystemState(System.nanoTime, theta0, 0.0))
+  val sensor = system.actorOf(Props(classOf[Sensor], PerfectModel, world))
+  val echo = system.actorOf(Props[SensorEcho])
+  sensor ! Sensor.Subscribe(echo)
+
+  // Wait for user input
+  readLine()
+  sensor ! Sensor.Unsubscribe(echo)
+  system.shutdown()
+}
+
+class SensorEcho extends Actor {
+  def receive = {
+    case Sensor.SensorReading(t, theta, sigTheta) ⇒
+      println(s"$t  $theta")
+  }
+}
